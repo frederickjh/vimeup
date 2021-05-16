f49ce02791b1525b6ba1191d4c79ae4a1288ea3a
Frederick Henderson <frederickjh@henderson-meier.org>
2021-05-17 00:42:00 +0200

    Started work on allowing Vimeup to be translated to other languages.

--- vimeup.py
+++ vimeup.py
@@ -9,6 +9,7 @@ from validate import Validator
 import vimeo
 import sys
 import webbrowser
+import gettext
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -25,20 +26,20 @@ def check_private_config():
 
     if result != True:
         sg.theme('LightBlue')
-        layout = [[sg.Text('The validation of the private configuration failed. This contains the authorization details for the Vimeo API. Without being able to authorize there is not much this program can do.')],
-                  [sg.Text('Would you like to fix the private configuration? Or exit? ')],
-                  [sg.Button('Yes'), sg.Button('Exit')]]
+        layout = [[sg.Text(t('The validation of the private configuration failed. This contains the authorization details for the Vimeo API. Without being able to authorize there is not much this program can do.'))],
+                  [sg.Text(t('Would you like to fix the private configuration? Or exit? '))],
+                  [sg.Button(t('Yes'),key='yes'), sg.Button(t('Exit'), key='exit')]]
         # Create the Window
-        window = sg.Window('Vimeup - Private configuration validation failed!', layout, font=["Arial", 12])
+        window = sg.Window(t('Vimeup - Private configuration validation failed!'), layout, font=["Arial", 12])
         # Event Loop to process "events" and get the "values" of the inputs
         while True:
             event, values = window.read()
-            if event == 'Yes':
+            if event == 'yes':
                 window.close()
                 private_config_setup()
                 check_private_config()
                 break
-            if event == sg.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks cancel
+            if event == sg.WIN_CLOSED or event == 'exit':  # if user closes window or clicks cancel
                 sys.exit(1)
                 break
         window.close()
@@ -64,25 +65,25 @@ def private_config_setup():
         personal_access_token_prefill = ""
         password_character = ""
     sg.theme('LightBlue')
-    layout = [[sg.Text('On the Vimeo Developer site you can copy the Client Identifier and Client Secrets. You can also generate a Personal Access Token.'),sg.Button('Open Vimeo developer site in a web browser', key='-to-vimeo-')],
-                [sg.Text('Vimeup Client ID', size=(30, 1)), sg.InputText(size=(40, 1), key='client_identifier', default_text=client_identifier_prefill, tooltip="Enter the client identifier for Vimeup."), sg.Text('40 Characters')],
-                [sg.Text('Vimeup Client secret', size=(30, 1), justification='left'), sg.InputText(size=(135, 1), key='client_secret', default_text=client_secret_prefill, tooltip="Enter the client secret for Vimeup."), sg.Text('128 Characters')],
-                [sg.Text('Vimeup Personal Access Token', size=(30, 1)), sg.InputText(size=(38, 1), key='personal_access_token', default_text=personal_access_token_prefill, password_char=password_character, tooltip="Enter the personal access token for Vimeup."), sg.Text('32 Characters'), sg.Text("Generate a new personal access token on Vimeo if you need one.", text_color="red")],
-                [sg.Button('OK'), sg.Button('Cancel')]]
+    layout = [[sg.Text(t('On the Vimeo Developer site you can copy the Client Identifier and Client Secrets. You can also generate a Personal Access Token.')),sg.Button(t('Open Vimeo developer site in a web browser'), key='-to-vimeo-')],
+                [sg.Text(t('Vimeup Client ID'), size=(30, 1)), sg.InputText(size=(40, 1), key='client_identifier', default_text=client_identifier_prefill, tooltip=t("Enter the client identifier for Vimeup.")), sg.Text(t('40 Characters'))],
+                [sg.Text(t('Vimeup Client secret'), size=(30, 1), justification='left'), sg.InputText(size=(135, 1), key='client_secret', default_text=client_secret_prefill, tooltip=t("Enter the client secret for Vimeup.")), sg.Text(t('128 Characters'))],
+                [sg.Text(t('Vimeup Personal Access Token'), size=(30, 1)), sg.InputText(size=(38, 1), key='personal_access_token', default_text=personal_access_token_prefill, password_char=password_character, tooltip=t("Enter the personal access token for Vimeup.")), sg.Text(t('32 Characters')), sg.Text(t("Generate a new personal access token on Vimeo if you need one."), text_color="red")],
+                [sg.Button(t('OK'), key='ok'), sg.Button(t('Cancel'), key='cancel')]]
     # Create the Window
-    window = sg.Window('Vimeup - Private Configuration Setup', layout, font=["Arial", 12])
+    window = sg.Window(t('Vimeup - Private Configuration Setup'), layout, font=["Arial", 12])
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
         if event == '-to-vimeo-':
-            popupreturned=sg.popup('I will now start a web browser and open the Vimeo developer site so you can get or create the required private configuration for this program, Vimeup.', r'Click OK and the browser will open', title='Vimeup - Launch Vimeo Developer site in web browser?', line_width=70, font=["Arial", 12])
+            popupreturned=sg.popup(t('I will now start a web browser and open the Vimeo developer site so you can get or create the required private configuration for this program, Vimeup.'), t('Click OK and the browser will open'), title=t('Vimeup - Launch Vimeo Developer site in web browser?'), line_width=70, font=["Arial", 12])
             if popupreturned == 'OK':
                 # Open web browser window.
-                webbrowser.open(r'https://developer.vimeo.com/apps/209908')
-        if event == 'OK':
+                webbrowser.open('https://developer.vimeo.com/apps/209908')
+        if event == 'ok':
             private_config_write(**values)
             break
-        if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        if event == sg.WIN_CLOSED or event == 'cancel':  # if user closes window or clicks cancel
             break
     window.close()
 
@@ -109,19 +110,19 @@ def vimeo_test_authentication():
         error_dict=response.json()
         unpack_error_message(**error_dict)
         sg.theme('LightBlue')
-        layout = [[sg.Text('We were unable to authenticate or connect to Vimeo.', size=(50, 1))],
-                  [sg.Text('The HTTP response code returned was:'), sg.Text(str(response.status_code), text_color="red")],
-                  [sg.Text('The error message was:')],
+        layout = [[sg.Text(t('We were unable to authenticate or connect to Vimeo.'), size=(50, 1))],
+                  [sg.Text(t('The HTTP response code returned was:')), sg.Text(str(response.status_code), text_color="red")],
+                  [sg.Text(t('The error message was:'))],
                   [sg.Text(error_msg, text_color="red")],
-                  [sg.Button('OK'), sg.Button('Cancel')]]
+                  [sg.Button(t('OK'), key='ok'), sg.Button(t('Cancel'), key='cancel')]]
         # Create the Window
-        window = sg.Window('Vimeup - Authentication / Connection to Vimeo failed', layout, font=["Arial", 12])
+        window = sg.Window(t('Vimeup - Authentication / Connection to Vimeo failed'), layout, font=["Arial", 12])
         # Event Loop to process "events" and get the "values" of the inputs
         while True:
             event, values = window.read()
-            if event == 'OK':
+            if event == 'ok':
                 break
-            if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+            if event == sg.WIN_CLOSED or event == 'cancel':  # if user closes window or clicks cancel
                 break
         window.close()
 
@@ -136,19 +137,19 @@ def check_local_config():
     # Check if the local configuration file exists...
     if not os.path.isfile(local_config) or os.stat(local_config).st_size == 0 :
         sg.theme('LightBlue')
-        layout = [[sg.Text('The local configuration file either does not exist or is empty.')],
-                  [sg.Text('Would you like setup the local configuration? Or use the default?')],
-                  [sg.Button('Yes'), sg.Button('Use Default local configuration')]]
+        layout = [[sg.Text(t('The local configuration file either does not exist or is empty.'))],
+                  [sg.Text(t('Would you like to setup the local configuration? Or use the default?'))],
+                  [sg.Button(t('Yes'), key='yes'), sg.Button(t('Use Default local configuration'), key='default')]]
         # Create the Window
         window = sg.Window('Vimeup - Local configuration file missing or empty.', layout, font=["Arial", 12])
         # Event Loop to process "events" and get the "values" of the inputs
         while True:
             event, values = window.read()
-            if event == 'Yes':
+            if event == 'yes':
                 window.close()
                 local_config_setup()
                 break
-            if event == sg.WIN_CLOSED or event == 'Use Default local configuration':  # if user closes window or choses to use the default local configuration
+            if event == sg.WIN_CLOSED or event == 'default':  # if user closes window or choses to use the default local configuration
                 localconfig['shared_configuration_file'] = './shared.ini'
                 localconfig['upload_directory'] = './'
                 localconfig['download_directory'] = './'
@@ -176,19 +177,19 @@ def local_config_setup():
     except:
         download_directory_prefill = ""
     sg.theme('LightBlue')
-    layout = [[sg.Text('Vimeup shared configuration file', size=(30, 1)), sg.InputText(size=(40, 1), key='shared_configuration_file', default_text=shared_configuration_file_prefill, tooltip="This is the location and name of the shared Vimeup configuration file. This can be shared via a file synchronization service."),sg.FileSaveAs(button_text="Choose File", file_types=(("Configuration Files", "*.ini"),("ALL Files", "*.*")), initial_folder="./", default_extension=".ini", key='toss1')],
-                [sg.Text('Vimeup upload directory', size=(30, 1), justification='left'), sg.InputText(size=(40, 1), key='upload_directory', default_text=upload_directory_prefill, tooltip="This is the directory Vimeup will first open when you upload a video."),sg.FolderBrowse(button_text="Choose Directory", initial_folder="./", key='toss2')],
-                [sg.Text('Vimeup download directory', size=(30, 1)), sg.InputText(size=(40, 1), key='download_directory', default_text=download_directory_prefill, tooltip="This is the directory Vimeup will first open when you a download video."),sg.FolderBrowse(button_text="Choose Directory", initial_folder="./", key='toss3')],
-                [sg.Button('OK'), sg.Button('Cancel')]]
+    layout = [[sg.Text(t('Vimeup shared configuration file'), size=(30, 1)), sg.InputText(size=(40, 1), key='shared_configuration_file', default_text=shared_configuration_file_prefill, tooltip=t("This is the location and name of the shared Vimeup configuration file. This can be shared via a file synchronization service.")),sg.FileSaveAs(button_text=t("Choose File"), file_types=((t("Configuration Files"), "*.ini"),(t("ALL Files"), "*.*")), initial_folder="./", default_extension=".ini", key='toss1')],
+                [sg.Text(t('Vimeup upload directory'), size=(30, 1), justification='left'), sg.InputText(size=(40, 1), key='upload_directory', default_text=upload_directory_prefill, tooltip=t("This is the directory Vimeup will first open when you upload a video.")),sg.FolderBrowse(button_text=t("Choose Directory"), initial_folder="./", key='toss2')],
+                [sg.Text(t('Vimeup download directory'), size=(30, 1)), sg.InputText(size=(40, 1), key='download_directory', default_text=download_directory_prefill, tooltip=t("This is the directory Vimeup will first open when you a download video.")),sg.FolderBrowse(button_text=t("Choose Directory"), initial_folder="./", key='toss3')],
+                [sg.Button(t('OK'), key='ok'), sg.Button(t('Cancel'), key='cancel')]]
     # Create the Window
-    window = sg.Window('Vimeup - Local Configuration Setup', layout, font=["Arial", 12])
+    window = sg.Window(t('Vimeup - Local Configuration Setup'), layout, font=["Arial", 12])
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
-        if event == 'OK':
+        if event == 'ok':
             local_config_write(**values)
             break
-        if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        if event == sg.WIN_CLOSED or event == 'cancel':  # if user closes window or clicks cancel
             break
     window.close()
 
@@ -202,6 +203,10 @@ def local_config_write(shared_configuration_file, upload_directory, download_dir
 
 
 # ###### MAIN PROGRAM #######
+# Multilingual Support
+localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
+translate = gettext.translation('vimeup', localedir, fallback=True)
+t = translate.gettext  # t is for translate
 # Private Configuration check and setup
 private_config = './private.ini'
 privateconfig = ConfigObj(private_config, configspec='./privatespec.ini')
