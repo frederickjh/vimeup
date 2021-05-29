5f6ab3b5aa5ba5a5bc02618dc5736f2864b99c61
Frederick Henderson <frederickjh@henderson-meier.org>
2021-05-29 20:50:59 +0200

    Lots of work on packaging script. Removed the need to specify on the command line the name of the python script and basename for executables, as we have specialized this script for Vimeup. Added some color to echo statements. Added Linux build to the script. Gave each of the Windows and Linux builds there own folders. Added Icons to the builds. Not sure if this is supported in the Linux build.

--- package.sh
+++ package.sh
@@ -2,15 +2,32 @@
 # Load configuration
 source package_config.sh
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 /path/to/main.py ProjectName"
-    exit 1
-else
-    FULL_PY_PATH=`readlink -f $1`
-    SOURCE_DIR_LINUX=`dirname ${FULL_PY_PATH}`
-    MAIN_PY=`basename ${FULL_PY_PATH}`
-    PROJECT_NAME="$2"
-fi
+# Set the Python program to make executables from and the basename of the executable.
+python_program=vimeup.py
+executable_name=vimeup
+
+    FULL_PY_PATH=$(readlink -f $python_program)
+    SOURCE_DIR_LINUX=$(dirname ${FULL_PY_PATH})
+    MAIN_PY=$(basename ${FULL_PY_PATH})
+    PROJECT_NAME="$executable_name"
+
+function loadcolor(){
+# Colors  http://wiki.bash-hackers.org/snipplets/add_color_to_your_scripts
+# More info about colors in bash http://misc.flogisoft.com/bash/tip_colors_and_formatting
+esc_seq="\x1b["  #In Bash, the <Esc> character can be obtained with the following syntaxes:  \e  \033  \x1B
+NC=$esc_seq"39;49;00m" # NC = Normal Color
+# Colors with black background (40;)set for emails.
+red=$esc_seq"31;40;01m"
+green=$esc_seq"32;40;00m"
+yellow=$esc_seq"33;40;01m"
+blue=$esc_seq"34;40;01m"
+magenta=$esc_seq"35;40;01m"
+cyan=$esc_seq"36;40;01m"
+}
+loadcolor
+
+echo -e "${yellow}>> ${green}Now building the ${cyan}Windows${green} executable for ${blue}$PROJECT_NAME${green}.${NC}"
+echo ""
 
 THIS_SCRIPT_PATH=`readlink -f $0`
 THIS_SCRIPT_DIR=`dirname ${THIS_SCRIPT_PATH}`
@@ -41,15 +58,13 @@ else
 
 fi
 
-BUILD_DIR_LINUX=${WINEPREFIX}/drive_c/build
-BUILD_DIR_WIN="C:\\build"
+BUILD_DIR_LINUX=${WINEPREFIX}/drive_c/win-build
+BUILD_DIR_WIN="C:\\win-build"
 mkdir -p ${BUILD_DIR_LINUX}
 
 # Create symbolic link to source directory so Windows can access it
 ln -s ${SOURCE_DIR_LINUX} ${BUILD_DIR_LINUX}/src_symlink
 SOURCE_DIR_WIN=${BUILD_DIR_WIN}\\src_symlink
-
-set -x
 
 # PIP FETCH
 echo builddir is ${BUILD_DIR_LINUX}
@@ -81,9 +96,12 @@ wine "${PYTHON_EXE_WIN}" "-m" "PyInstaller" \
     --onedir \
     --noconsole \
     --noconfirm \
+    --icon=logo/vimeup.ico \
     --add-data "privatespec.ini;." \
     --add-data "localspec.ini;." \
     --add-data "locale;locale" \
+    --add-data "logo/vimeup.ico;logo" \
+    --add-data "logo/vimeup.png;logo" \
     --distpath "win-dist" \
     --workpath "win-build" \
     "${SOURCE_DIR_WIN}\\${MAIN_PY}"
@@ -91,5 +109,27 @@ wine "${PYTHON_EXE_WIN}" "-m" "PyInstaller" \
 rm -rf ${WINEPREFIX}
 mkdir -p win-dist/${PROJECT_NAME}/vimeo-configuration
 
-echo "Executable available at win-dist/${PROJECT_NAME}/${PROJECT_NAME}.exe"
+echo ""
+echo -e "${yellow}>> ${green}The ${cyan}Windows${green} executable available at ${blue}win-dist/${PROJECT_NAME}/${PROJECT_NAME}.exe${green}.${NC}"
+echo ""
 
+
+echo -e "${yellow}>> ${green}Now building the ${cyan}Linux${green} executable for ${blue}$PROJECT_NAME${green}.${NC}"
+echo ""
+
+pyinstaller --onedir \
+            --noconsole \
+            --noconfirm \
+            --icon=logo/vimeup.png \
+            --add-data "privatespec.ini:." \
+            --add-data "localspec.ini:." \
+            --add-data "locale:locale" \
+            --add-data "logo/vimeup.png:logo" \
+            --distpath "lin-dist" \
+            --workpath "lin-build" \
+            vimeup.py
+mkdir -p lin-dist/${PROJECT_NAME}/vimeo-configuration
+
+echo ""
+echo -e "${yellow}>> ${green}The ${cyan}Linux${green} executable available at ${blue}lin-dist/${PROJECT_NAME}/${PROJECT_NAME}${green}.${NC}"
+echo ""
